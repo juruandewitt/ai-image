b623dfe393f5f9cc046fb10893dfd2951c914a4c
juruandewitt <juruandewitt@gmail.com>
2025-11-26 20:17:27 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,10 @@
+// next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '*.public.blob.vercel-storage.com',
-      },
+      { protocol: 'https', hostname: '*.public.blob.vercel-storage.com' },
     ],
-    // Optionally allow inline data: URIs if you ever use them
-    formats: ['image/avif', 'image/webp'],
   },
 }
 
